7b3dbe1a6585bbd2024f9652dfa1dda099b3d3ed
Pavania-Rahul <rahulpavania@gmail.com>
2026-07-13 18:41:43 +0530

    Modified File

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,52 +1,42 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  testDir: './tests',
+
+  fullyParallel: false,
+
+  retries: process.env.CI ? 2 : 0,
+
+  workers: process.env.CI ? 1 : undefined,
+
+  reporter: [
+    ['list'],
+    ['html', { 
+      outputFolder: 'playwright-report',
+      open: 'never'
+    }]
+  ],
+
+  use: {
+    headless: !!process.env.CI,
+
     trace: 'on-first-retry',
-    headless:true
+
+    screenshot: 'only-on-failure',
+
+    video: 'retain-on-failure'
   },
 
-  /* Configure projects for major browsers */
+
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+      use: {
+        ...devices['Desktop Chrome']
+      }
+    }
+  ]
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    
-  ],
-
-  
 });
